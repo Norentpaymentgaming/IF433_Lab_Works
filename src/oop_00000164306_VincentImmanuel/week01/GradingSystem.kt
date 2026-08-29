f11046da5166bc597515caa893b703bd1c184700
unknown <vince10000265@gmail.com>
2026-08-29 11:41:22 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000164306_VincentImmanuel/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000164306_VincentImmanuel.week01 // Make sure the package is in accordance
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
