14fe9737cc5ae7b6020e377ecd276275f1387c0e
Your Name <you@example.com>
2026-09-02 20:59:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000164306_VincentImmanuel/week01/GameStore.kt
+++ src/oop_00000164306_VincentImmanuel/week01/GameStore.kt
@@ -4,3 +4,9 @@ fun main() {
     val gameTitle = "Minecraft"
     val price = 600000
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    price * 20 / 100
+} else {
+    price * 10 / 100
+}
