ac766aabec129078a3d8ba033b3e24094dd26555
Your Name <you@example.com>
2026-09-02 20:56:38 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000164306_VincentImmanuel/week01/CircleCalculator.kt
+++ src/oop_00000164306_VincentImmanuel/week01/CircleCalculator.kt
@@ -1,18 +1,13 @@
 package oop_00000164306_VincentImmanuel.week01
 
-fun main(args: Array<String>) {
-
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     var area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius, Area: $area")
 
-    // Logic check
     println(checkSize(area))
 }
 
