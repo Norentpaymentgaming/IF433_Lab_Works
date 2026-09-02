6fec9a449d523db86e4439f5450c6f835cead161
Your Name <you@example.com>
2026-09-02 21:00:23 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000164306_VincentImmanuel/week01/GameStore.kt
+++ src/oop_00000164306_VincentImmanuel/week01/GameStore.kt
@@ -3,10 +3,20 @@ package oop_00000164306_VincentImmanuel.week01
 fun main() {
     val gameTitle = "Minecraft"
     val price = 600000
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    println("Judul: $gameTitle")
+    println("Harga Asli: Rp$price")
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) {
     price * 20 / 100
 } else {
     price * 10 / 100
+}
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Harga Akhir: Rp$finalPrice")
 }
