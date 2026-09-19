1730546358acc9b503faa19e7625a75240ebe7e0
Your Name <you@example.com>
2026-09-19 10:17:44 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_00000164306_VincentImmanuel/week04/Manager.kt
@@ -0,0 +1,12 @@
+package oop_00000164306_VincentImmanuel.week04
+
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500000
+    }
+}
